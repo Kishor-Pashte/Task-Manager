42fd119d77f401118fb99923e570376a83689ca2
KISHOR PASHTE <k24145798@gmail.com>
2026-04-08 00:42:54 +0530

    Todo mobile frontend done

--- TodoAppByHarshalSir/app/index.tsx
+++ TodoAppByHarshalSir/app/index.tsx
@@ -1,11 +1,23 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Text } from "@react-navigation/elements";
-import { SafeAreaViewBase } from "react-native";
+import Checkbox from "expo-checkbox";
+import { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  SafeAreaViewBase,
+  TouchableOpacity,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 import {
   FlatList,
   View,
 } from "react-native-reanimated/lib/typescript/Animated";
 
+type TodoType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 export default function Index() {
   const todoData = [
     {
@@ -40,6 +52,46 @@ export default function Index() {
     },
   ];
 
+  const [todos, setTodos] = useState<TodoType[]>(todoData);
+  const [input, setInput] = useState<string>("");
+
+  //fetch todos
+  // const fetchTodos = async () => {
+  //   const tasks = await axios.get("/todos");
+  //   setTodos(tasks);
+  // };
+
+  // useEffect(() => {
+  //   fetchTodos();
+  // }, []);
+
+  // add todos function
+  const addTodo = async () => {
+    const newTodo = {
+      id: Math.random(),
+      title: input,
+      isDone: false,
+    };
+
+    //***call create todo backend api
+    //const tasks = await axios.post('/todos', newTodo);
+
+    //setTodos(tasks)
+  };
+
+  //mark done
+  // const markAsDone = async () => {
+  //   await axios.put("/todos/:id");
+
+  //   fetchTodos();
+  // };
+
+  //delete todo
+  // const deleteTodo = async () => {
+  //   await axios.delete("/todos/:id");
+  //   fetchTodos();
+  // };
+
   return (
     <SafeAreaViewBase
       style={{
@@ -61,15 +113,106 @@ export default function Index() {
         <Ionicons name="menu" size={24} color={"#333"} />
         <Text>Todo App</Text>
       </View>
+
+      {/* //search bar */}
+      <View
+        style={{
+          backgroundColor: "#fff",
+          padding: 16,
+          borderRadius: "10",
+          gap: "10",
+        }}
+      >
+        <Ionicons name="search" size={24} color={"#333"} />
+        <TextInput
+          placeholder="Search"
+          style={{ flex: 1, fontSize: 16, color: "#333" }}
+        />
+      </View>
+
       <FlatList
-        data={todoData}
+        data={todos.reverse()}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.title}</Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              backgroundColor: "#fff",
+              padding: 16,
+              borderRadius: 10,
+              marginBottom: 20,
+            }}
+          >
+            <View style={{ gap: 10, alignItems: "center" }}>
+              <Checkbox value={item.isDone} />
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: "#333",
+                  textDecorationColor: item.isDone ? "line-through" : "none",
+                }}
+              >
+                {item.title}
+              </Text>
+            </View>
+            <TouchableOpacity
+              onPress={() => {
+                alert("Deleted" + item.id);
+              }}
+            >
+              <Ionicons
+                name="trash"
+                size={24}
+                color={"red"}
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              />{" "}
+            </TouchableOpacity>
           </View>
         )}
       />
+
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Add new todo"
+          onChangeText={(text) => setInput(text)}
+          value={input}
+          style={{
+            display: "flex",
+            flex: 1,
+            backgroundColor: "#fff",
+            padding: 16,
+            borderRadius: 10,
+            fontSize: 16,
+            color: "#333",
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            addTodo();
+          }}
+          style={{
+            backgroundColor: "#2d1abc",
+            padding: 8,
+            borderRadius: 10,
+            marginLeft: 20,
+          }}
+        >
+          <Ionicons name="add" size={32} color={"#fff"} />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </SafeAreaViewBase>
   );
 }
